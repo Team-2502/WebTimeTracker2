c90906d725c1d70acff683db6f7d9c0f98284e4f
Ryan Alexander <ryandalex1@outlook.com>
2020-07-02 16:22:09 -0500

    Make sure name of new user consists of letters

--- members/forms.py
+++ members/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.core import validators
 
 
 class NewMemberForm(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=20)
-    last_name = forms.CharField(label='Last Name', max_length=20)
+    first_name = forms.CharField(label='First Name', max_length=20, validators=[validators.RegexValidator(regex='^[a-zA-Z/s]+$', message="Only letters allowed!")])
+    last_name = forms.CharField(label='Last Name', max_length=20, validators=[validators.RegexValidator(regex='^[a-zA-Z/s]+$', message="Only letters allowed!")])
