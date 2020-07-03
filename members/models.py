30821a97bc8923500bf79e4f70a5fff98b037353
Ryan Alexander <ryandalex1@outlook.com>
2020-07-02 19:02:17 -0500

    Attempt to fix error with datetimes in Postgres

--- members/models.py
+++ members/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+from datetime import datetime
 
 
 class Member(models.Model):
@@ -7,7 +7,7 @@ class Member(models.Model):
     last_name = models.CharField(max_length=20)
     num_hours = models.DecimalField(default=0, max_digits=5, decimal_places=2)
     logged_in = models.BooleanField(default=False)
-    sign_in_time = models.DateTimeField(default=timezone.now)
+    sign_in_time = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
         return str.capitalize(self.first_name) + " " + str.capitalize(self.last_name)
