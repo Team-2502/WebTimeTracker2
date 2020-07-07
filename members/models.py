49bae958aba3284b8ceacea055279a0ef36d9da1
Ryan Alexander <ryandalex1@outlook.com>
2020-07-07 18:34:46 -0500

    Reorder homepage links to increase usability

--- members/models.py
+++ members/models.py
@@ -5,7 +5,6 @@ from datetime import datetime
 class Member(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    # This causes admin page to break if blank is not True, should fix on master
     team_role = models.CharField(max_length=25, default='', blank=True)
     num_hours = models.DecimalField(default=0, max_digits=5, decimal_places=2)
     logged_in = models.BooleanField(default=False)
